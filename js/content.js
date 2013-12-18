91e02ec8681047f0d077795169815cb087e4c7fe
jarquafelmu <thechaoselement@gmail.com>
2013-12-17 19:44:04 -0700

    Added functionality for the sidebar container's height to be dynamically calculated on page load. This is used to allow the gray background to flow all the way from the header to the footer.

--- js/content.js
+++ js/content.js
@@ -1,10 +1,24 @@
 $(document).ready(function() {
 	//adjust_sideNav();
-	define_offcanvas_datatoggle();
+	/*define_offcanvas_datatoggle();*/
 	set_affix_values();
+	set_sidebar_height();
 });
 
-function adjust_sideNav () {
+function set_sidebar_height () {
+	var footer = $('footer').outerHeight(true);
+	var header = $('header').outerHeight(true);
+	var page = $(document).outerHeight(true);	
+	var sideBarHeight = page - header - footer;
+	
+	$('#sidebar').height(sideBarHeight);
+}
+
+function showHeight( element, height ) {
+	alert("The height for the " + element + " is " + height + "px.");
+}
+
+/*function adjust_sideNav () {
 	var sideNav = 'ul.pf-sidenav > li:has(ul.nav)';
 	assign_carets(sideNav + ' > a');
 	//assign_dropdown_behavior(sideNav);
@@ -29,19 +43,19 @@ function define_offcanvas_datatoggle () {
 	$('[data-toggle=offcanvas]').click(function() {
 		$('.row-offcanvas').toggleClass('active');
 	});
-}
+} */
 
 function set_affix_values () {
 	/*$('#sub_nav').affix({
 			offset: { top: $('#sub_nav').offset().top }
 	});*/
-	
-	$('#sideNav').affix({
-	offset: {
-		top: 200
-		, bottom: function () {
-				return (this.bottom = $('footer').outerHeight(true))
-			}
+	var top = "Sidebar Top: " + $('#sidebar').offset().top;
+	var bottom = "Footer: " + $('footer').outerHeight(true);
+	var header = "header: " + $('header').outerHeight(true);
+	//alert(top + "\n" + bottom + "\n" + header);
+	$('#sideNav').affix({		
+		offset: {
+			top: 100, 
 		}
 	});
 	
